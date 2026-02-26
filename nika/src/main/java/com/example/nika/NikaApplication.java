97553df382fd6bc7b6108b431ad4c5fe77d1a992
Nicolas Kauan <166877999+NicolasKauan@users.noreply.github.com>
2026-02-26 19:21:16 -0300

    primeiro commit

---
+++ nika/src/main/java/com/example/nika/NikaApplication.java
@@ -0,0 +1,13 @@
+package com.example.nika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NikaApplication.class, args);
+	}
+
+}
---
+++ nika/src/test/java/com/example/nika/NikaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
